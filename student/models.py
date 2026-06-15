28cb382d471aaaf88e76e62af3c4f6645ec37fdf
javadkalamati <kalematijavad@gmail.com>
2026-06-16 00:22:16 +0330

    first model person created

--- student/models.py
+++ student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    family = models.CharField(max_length=100)
+    mellinumber = models.IntegerField(max_length=100,unique=True)
+    address = models.TextField()
+    birthday = models.DateField()
+    
+
+
 # Create your models here.
--- student/views.py
+++ student/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Person
 
 
 
 def home(request):
-    return render(request,'home.html')
+    students=Person.objects.all()
+    return render(request,'home.html',{'student':students})
 
 # Create your views here.
