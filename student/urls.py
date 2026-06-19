baa1194b64a6f8bafe921ae43d755346b6c2772d
javadkalamati <kalematijavad@gmail.com>
2026-06-19 15:26:39 +0330

    detail page created bootstrp and table activated

--- student/urls.py
+++ student/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path("home/",views.home),
+    path("details/<int:person_id>/",views.detail),
 ]
--- student/views.py
+++ student/views.py
@@ -7,4 +7,7 @@ def home(request):
     students=Person.objects.all()
     return render(request,'home.html',{'student':students})
 
+def detail(request,person_id):
+    student = Person.objects.get(id=person_id)
+    return render(request,'details.html',{"student":student})
 # Create your views here.
